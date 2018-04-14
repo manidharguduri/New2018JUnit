2c743dacbe9febd5d9cc4b89a4de3dd4c975a826
Manidhar.Guduri <Manidhar.Guduri@target.com>
2018-04-14 14:51:48 +0530

    Array : Reverse : Util with Test cases

--- src/main/array/ReverseArrayUtil.java
+++ src/main/array/ReverseArrayUtil.java
@@ -1,4 +1,39 @@
 package array;
 
 public class ReverseArrayUtil {
+
+    public static int[] reverseArray(int[] arr) {
+
+        if (arr == null)
+            return null;
+
+        if (arr.length == 1)
+            return arr;
+
+        if (arr.length == 2 || arr.length == 3) {
+            swap(arr, 0, arr.length - 1);
+            return arr;
+        }
+
+        int firstIndex1 = 0;
+        int lastIndex1 = arr.length-1;
+        while (firstIndex1 < lastIndex1) {
+            int temp = arr[firstIndex1];
+            arr[firstIndex1] = arr[lastIndex1];
+            arr[lastIndex1] = temp;
+            firstIndex1++;
+            lastIndex1--;
+        }
+
+        return arr;
+
+    }
+
+    private static void swap(int[] arr, int startPoint, int destinationPoint) {
+
+        int temp = arr[startPoint];
+        arr[startPoint] = arr[destinationPoint];
+        arr[destinationPoint] = temp;
+
+    }
 }
--- src/test/array/ReverseArrayUtilTest.java
+++ src/test/array/ReverseArrayUtilTest.java
@@ -1,4 +1,52 @@
 package array;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ReverseArrayUtilTest {
+
+
+    @Test
+    public void testingWithNullValue()
+    {
+        int[] input = null;
+        int[] expectedResult = ReverseArrayUtil.reverseArray(input );
+        Assert.assertEquals(expectedResult,null);
+    }
+
+    @Test
+    public void testingWithOneArrayValue()
+    {
+        int[] input = {1};
+        int[] actualResult = {1};
+        int[] expectedResult = ReverseArrayUtil.reverseArray(input );
+        Assert.assertArrayEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void testingWithTwoArrayValue()
+    {
+        int[] input = {1,2};
+        int[] actualResult = {2,1};
+        int[] expectedResult = ReverseArrayUtil.reverseArray(input );
+        Assert.assertArrayEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void testingWithThreeArrayValue()
+    {
+        int[] input = {1,2,3};
+        int[] actualResult = {3,2,1};
+        int[] expectedResult = ReverseArrayUtil.reverseArray(input );
+        Assert.assertArrayEquals(expectedResult,actualResult);
+    }
+
+    @Test
+    public void testingWithMultipleArrayValue()
+    {
+        int[] input = {1,2,3,4,5};
+        int[] actualResult = {5,4,3,2,1};
+        int[] expectedResult = ReverseArrayUtil.reverseArray(input );
+        Assert.assertArrayEquals(expectedResult,actualResult);
+    }
 }
